b2daedccee975f7dc7fad064264a4f57e9fda520
pi <ibrahim.mohammad@isen-ouest.yncrea.fr>
2024-12-06 19:04:37 +0100

    pgflow

--- mlflow/entrypoint.sh
+++ mlflow/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-# # Attendre que PostgreSQL soit disponible
-# ./wait-for-it.sh postgres:${POSTGRES_PORT} -- echo "PostgreSQL is up"
+# Exporter les variables nécessaires pour l'intégration avec MinIO
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export MLFLOW_S3_ENDPOINT_URL=${MLFLOW_S3_ENDPOINT_URL:-http://minio:9000}
 
-# # Attendre que MinIO soit disponible
-# ./wait-for-it.sh minio:${MINIO_PORT} -- echo "MinIO is up"
+# Attendre que les dépendances soient prêtes
+/wait-for-it.sh postgres:${POSTGRES_PORT} -- echo "PostgreSQL is up"
+/wait-for-it.sh minio:${MINIO_PORT} -- echo "MinIO is up"
 
 # Lancer MLflow Server
 exec mlflow server \
