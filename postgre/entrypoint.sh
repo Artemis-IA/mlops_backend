875d0d2a9d190c0e91d3eb6e9ce0fd0f3738afb7
pi <ibrahim.mohammad@isen-ouest.yncrea.fr>
2024-12-08 21:52:29 +0100

    grafana

--- postgre/entrypoint.sh
+++ postgre/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+# Log environment variables for debugging
+echo "Vérification des variables d'environnement :"
+echo "POSTGRES_USER=${POSTGRES_USER}"
+echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}"
+echo "POSTGRES_DB=${POSTGRES_DB}"
+echo "MLFLOW_USER=${MLFLOW_USER}"
+echo "MLFLOW_PASSWORD=${MLFLOW_PASSWORD}"
+echo "MLFLOW_DB=${MLFLOW_DB}"
+echo "LABEL_STUDIO_EMAIL=${LABEL_STUDIO_EMAIL}"
+echo "LABEL_STUDIO_PASSWORD=${LABEL_STUDIO_PASSWORD}"
+echo "LABEL_STUDIO_DB=${LABEL_STUDIO_DB}"
+
 # Start PostgreSQL server
 docker-entrypoint.sh postgres "$@" &
 
@@ -20,7 +32,13 @@ done
 
 # Substitute environment variables in SQL template and create a processed SQL file
 if [ -f /docker-entrypoint-initdb.d/init_db.sql.template ]; then
+  echo "Substituting variables in init_db.sql.template..."
   envsubst < /docker-entrypoint-initdb.d/init_db.sql.template > /docker-entrypoint-initdb.d/init_db.sql
+  echo "Fichier SQL généré :"
+  cat /docker-entrypoint-initdb.d/init_db.sql
+else
+  echo "Template file init_db.sql.template not found!"
+  exit 1
 fi
 
 # Initialize the database using the processed SQL file
