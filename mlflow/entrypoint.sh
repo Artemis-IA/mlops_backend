54df4808a523579b6a9a6236a2b0ec55153c6257
pi <ibrahim.mohammad@isen-ouest.yncrea.fr>
2024-12-13 02:33:00 +0100

    viz

--- mlflow/entrypoint.sh
+++ mlflow/entrypoint.sh
@@ -7,8 +7,8 @@ export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export MLFLOW_S3_ENDPOINT_URL=${MLFLOW_S3_ENDPOINT_URL:-http://minio:9000}
 
 # Attendre que les dépendances soient prêtes
-/wait-for-it.sh postgres:${POSTGRES_PORT} -- echo "PostgreSQL is up"
-/wait-for-it.sh minio:${MINIO_PORT} -- echo "MinIO is up"
+/wait-for-it.sh ${POSTGRES_HOST}:${POSTGRES_PORT} -- echo "PostgreSQL is up"
+/wait-for-it.sh ${MINIO_HOST}:${MINIO_PORT} -- echo "MinIO is up"
 
 # Lancer MLflow Server
 exec mlflow server \
